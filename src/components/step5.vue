01032f2888afeaa82b61f46777720068ad5f48cc
JW Weatherman <jw@jwweatherman.com>
2019-11-12 11:42:18 -0500

    Additional instruction on step 5.

--- src/components/step5.vue
+++ src/components/step5.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 5: Download Yeti to your ONLINE laptop</h2>
-      <div>Click <a href="https://drive.google.com/uc?authuser=0&id=11ELudchViKE7OKVk0C7w7nzIhwDPhB2T&export=download" download> here</a> download Yeti and then click Next to continute.</div>
+      <div>You should be reading this on a freshly installed Ubuntu laptop labled "ONLINE" because you follewed the previous steps. Click <a href="https://drive.google.com/uc?authuser=0&id=11ELudchViKE7OKVk0C7w7nzIhwDPhB2T&export=download" download> here</a> download Yeti and then click Next to continute.</div>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
 </template>
