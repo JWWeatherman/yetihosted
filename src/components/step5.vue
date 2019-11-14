78465f692825ada2fe7207ac4174912c4fc1e548
Will Weatherman <will@mathbot.com>
2019-11-14 10:35:44 -0500

    Changed scripts

--- src/components/step5.vue
+++ src/components/step5.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 5: Download Yeti to your ONLINE laptop</h2>
-      <div>You should be reading this on a freshly installed Ubuntu laptop labled "ONLINE" because you follewed the previous steps. Click <a href="https://drive.google.com/uc?authuser=0&id=1FJ7Q0ZQFs2xrvMQenS3SyIcyIo1A2atk&export=download" download> here</a> download Yeti Recovery and then click Next to continute.</div>
+      <div>You should be reading this on a freshly installed Ubuntu laptop labled "ONLINE" because you follewed the previous steps. Click <a href="https://drive.google.com/uc?authuser=0&id=1PsAI7JE5bgQBZvl75LZhSIpnSa-oBLzp&export=download" download> here</a> download Yeti Recovery and then click Next to continute.</div>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
 </template>
--- src/components/step52.vue
+++ src/components/step52.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 5: Download Yeti to your ONLINE laptop</h2>
-      <div>You should be reading this on a freshly installed Ubuntu laptop labled "ONLINE" because you follewed the previous steps. Click <a href="https://drive.google.com/uc?authuser=0&id=1qarg_zhaHNYzLSz7CTGvCD2B9HH1U4MX&export=download" download> here</a> download Yeti and then click Next to continute.</div>
+      <div>You should be reading this on a freshly installed Ubuntu laptop labled "ONLINE" because you follewed the previous steps. Click <a href="https://drive.google.com/uc?authuser=0&id=1-wWoKn5NLKUNms8HetRZFfLB4uwXl2CW&export=download" download> here</a> download Yeti and then click Next to continute.</div>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
 </template>
