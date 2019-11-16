acd0167c74bd7afbe1d702259ab449a843db5e09
Will Weatherman <will@mathbot.com>
2019-11-16 15:45:46 -0500

    color changer

--- src/components/YetiColdStartup5.vue
+++ src/components/YetiColdStartup5.vue
@@ -5,14 +5,14 @@
           <h1>Setup Yeti Cold Storage</h1>
           <p>Select this option if you want to create a new place to store bitcoin.</p>
           <p>
-            <input v-on:click="start" style="margin-bottom:50px;" class="btn btn-danger btn-large" type="submit" id="next" value="Learn more">
+            <input v-on:click="start" style="margin-bottom:50px;" class="btn btn-primary btn-large" type="submit" id="next" value="Learn more">
           </p>
       </div>
       <div class="hero-unit">
           <h1>Recover Bitcoin from Yeti Cold Storage</h1>
           <p>Select this option if you want to spend bitcoin out of your existing Yeti Cold Storage location and you have 3 of your 7 seed packets.</p>
           <p>
-            <input v-on:click="recover" style="margin-bottom:50px;" class="btn btn-danger btn-large" type="submit" id="next" value="Learn more">
+            <input v-on:click="recover" style="margin-bottom:50px;" class="btn btn-primary btn-large" type="submit" id="next" value="Learn more">
           </p>
       </div>
     </div>
