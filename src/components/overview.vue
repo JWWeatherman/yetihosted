918b38a3ebcc9adbfa83d0ac715c97ffcd967cc6
JW Weatherman <jw@jwweatherman.com>
2019-11-18 11:58:51 -0500

    Button names.

--- src/components/overview.vue
+++ src/components/overview.vue
@@ -5,21 +5,21 @@
           <h1>Yeti Cold Storage</h1>
           <p>Yeti Cold Storage is for storing more than $50,000 USD worth of bitcoin. It is a 3 of 7 HD multisig setup where private keys are never on an internet connected device.</p>
           <p>
-            <input v-on:click="Cold" style="margin-bottom:50px;" class="btn btn-primary btn-large" type="submit" id="next" value="Learn more">
+            <input v-on:click="Cold" style="margin-bottom:50px;" class="btn btn-primary btn-large" type="submit" id="next" value="Yeti Cold">
           </p>
       </div>
       <div class="hero-unit">
           <h1>Yeti Warm Storage</h1>
           <p>Yeti Warm Storage is for storing between $5,000 and $50,000 USD worth of bitcoin. It is a 3 of 7 HD multisig setup that requries only a single computer to setup.</p>
           <p>
-            <input v-on:click="Warm" style="margin-bottom:50px;" class="btn btn-warning btn-large" type="submit" id="next" value="Learn more">
+            <input v-on:click="Warm" style="margin-bottom:50px;" class="btn btn-warning btn-large" type="submit" id="next" value="Yeti Warm">
           </p>
       </div>
       <div class="hero-unit">
           <h1>Yeti Hot Storage</h1>
           <p>Yeti Hot Storage is for storing under $5,000 USD worth of bitcoin. It is very easy to backup and restore and requries only a single computer to setup.</p>
           <p>
-            <input v-on:click="Hot" style="margin-bottom:50px;" class="btn btn-danger btn-large" type="submit" id="next" value="Learn more">
+            <input v-on:click="Hot" style="margin-bottom:50px;" class="btn btn-danger btn-large" type="submit" id="next" value="Yeti Hot">
           </p>
       </div>
       <p>
