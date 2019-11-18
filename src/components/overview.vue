fafaa36539fba6742031f4a43c4cac530d2972e0
Will Weatherman <will@mathbot.com>
2019-11-18 09:33:33 -0500

    routing test

--- src/components/overview.vue
+++ src/components/overview.vue
@@ -32,11 +32,11 @@
 export default {
   mounted () {
     let host = window.location.host.toUpperCase()
-    if (host = 'COLD.YETICOLD.COM') {
+    if (host === 'COLD.YETICOLD.COM') {
       this.$router.push({path: '/Cold/step4'})
-    }else if (host = 'WARM.YETICOLD.COM') {
+    }else if (host === 'WARM.YETICOLD.COM') {
       this.$router.push({path: '/Warm/step4'})
-    }else if (host = 'HOT.YETICOLD.COM') {
+    }else if (host === 'HOT.YETICOLD.COM') {
       this.$router.push({path: '/Hot/step4'})
     }
   },
