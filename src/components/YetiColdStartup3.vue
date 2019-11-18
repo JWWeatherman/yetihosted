782af0b1994d184077bd001de218691967222f91
JW Weatherman <jw@jwweatherman.com>
2019-11-18 11:09:05 -0500

    Changed text for cold.yeticold.com

--- src/components/YetiColdStartup3.vue
+++ src/components/YetiColdStartup3.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 3: Switch to your ONLINE laptop</h2>
-      <div>On your ONLINE laptop go to http://cold.yeticold.com/ to continue to Step 4.</div>
+      <div>On your ONLINE laptop go to "cold.yeticold.com" to continue to Step 4.</div>
     </div>
 </template>
 
