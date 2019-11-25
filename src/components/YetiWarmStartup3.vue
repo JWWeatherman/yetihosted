9c44887710ff01d838d667d0b9e3284cee4f732e
JW Weatherman <jw@jwweatherman.com>
2019-11-25 07:51:42 -0800

    Text changes.

--- src/components/YetiWarmStartup3.vue
+++ src/components/YetiWarmStartup3.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
-      <h2>Step 3: Switch to your ONLINE laptop</h2>
-      <div>On your ONLINE laptop go to "warm.yeticold.com" to continue to Step 4.</div>
+      <h2>Step 3: Switch to your Ubuntu laptop</h2>
+      <div>On your freshly installed Ubuntu laptop go to "warm.yeticold.com" to continue to Step 4.</div>
     </div>
 </template>
