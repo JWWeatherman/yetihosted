efe0447af6b3c4d73956d1c0ce181316d3f30b81
JW Weatherman <jw@jwweatherman.com>
2019-11-25 11:01:40 -0500

    Text changes

--- src/components/YetiColdStartup1.vue
+++ src/components/YetiColdStartup1.vue
@@ -3,9 +3,9 @@
     <h2>Step 1: Gather Required Equipment</h2>
     <p>Gather the following items and then click next.</p>
       <ul>
-          <li>Two laptops that can run Ubuntu and Bitcoin Core. At the time of this writing almost any laptop with at least 500 GB of disk storage will work but if your laptop has an SSD type hard some steps will take a few hours instead of a few days. Laptops without SSD type drives can be purchased for less than $200 and SSD drives add roughly $50 to the cost.</li>
-          <li>An external hard drive with at least 500 GB of disk storage that can connect to both laptops. This will cost less about $100.</li>
-          <li>A second external hard drive with at least 10 GB of disk storage than can connect to both laptops. This will cost about $25.</li>
+          <li>Two laptops that can run Ubuntu and Bitcoin Core. At the time of this writing almost any laptop with at least 500 GB of disk storage will work but if your laptop has an SSD type hard some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD.</li>
+          <li>An external hard drive with at least 500 GB of disk storage that can connect to both laptops. This will cost less than $100.</li>
+          <li>8 small USB thumb drives. These will cost about $3 each and will be used to store your bitcoin seeds along with a paper copy.</li>
           <li>A printer and printer paper.</li>
       </ul>
     <input v-on:click="click" class="btn btn-primary" type="submit" id="next" value="Next">
