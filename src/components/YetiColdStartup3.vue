783f4c07e22fe2f50df1890a34663d9534eeaadb
JW Weatherman <jw@jwweatherman.com>
2019-11-25 11:08:40 -0500

    Text

--- src/components/YetiColdStartup3.vue
+++ src/components/YetiColdStartup3.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 3: Switch to your ONLINE laptop</h2>
-      <div>On your ONLINE laptop go to "cold.yeticold.com" to continue to Step 4.</div>
+      <div>On your ONLINE laptop open firefox and type the following URL into the address bar and click Enter.</div>
+      <code>cold.yeticold.com</code>
     </div>
 </template>
 
