d37b8571840c3b57a1b6aca8d15dced473f4fd8c
JW Weatherman <jw@jwweatherman.com>
2019-11-29 14:50:29 -0800

    Typo.

--- src/components/YetiWarmStartup2.vue
+++ src/components/YetiWarmStartup2.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 2: Install Ubuntu</h2>
-      <p>Install Ubuntu on your laptop and then click Next.:</p>
+      <p>Install Ubuntu on your laptop and then click Next.</p>
       <ul>
           <li>Use only trustworthy guides such as this one: <a link="https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0">https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0</a></li>
           <li>Delete and erase all existing partitions using the Disk utility that you can access by selecting "Try Ubuntu" before installing Ubuntu.</li>
