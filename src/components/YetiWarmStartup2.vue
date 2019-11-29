30947591dc333baa3179ce1dcfcac3fb33ec035d
JW Weatherman <jw@jwweatherman.com>
2019-11-29 14:57:51 -0800

    Text.

--- src/components/YetiWarmStartup2.vue
+++ src/components/YetiWarmStartup2.vue
@@ -3,9 +3,9 @@
       <h2>Step 2: Install Ubuntu</h2>
       <p>Install Ubuntu on your laptop and then click Next.</p>
       <ul>
-          <li>Use only trustworthy guides such as this one: <a link="https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0">https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0</a></li>
+          <li>Use only trustworthy guides such as this one: <a href="https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0">Install Ubuntu</a></li>
           <li>Delete and erase all existing partitions using the Disk utility that you can access by selecting "Try Ubuntu" before installing Ubuntu.</li>
-          <li>Use one of your USB drives to create a bootable Ubuntu drive</li>
+          <li>Use one of your USB drives to create a bootable Ubuntu drive.</li>
       </ul>
       <input v-on:click="click" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
