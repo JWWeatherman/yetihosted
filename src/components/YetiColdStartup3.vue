89e0a811a5bab3d09f008d682052018717bc0d1d
JW Weatherman <jw@jwweatherman.com>
2019-11-30 13:15:28 -0500

    Text

--- src/components/YetiColdStartup3.vue
+++ src/components/YetiColdStartup3.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
-      <h2>Step 3: Switch to your <b>Online</b> laptop</h2>
-      <div>On your ONLINE laptop open firefox and type the following URL into the address bar and click <b>Enter</b>.</div>
+      <h2>Step 3: Switch to your Online laptop</h2>
+      <div>On your <b>Online</b> laptop open Firefox and type the following URL into the address bar and click <b>Enter</b>.</div>
       <div></div>
       <code>cold.yeticold.com</code>
     </div>
--- src/components/YetiWarmStartup3.vue
+++ src/components/YetiWarmStartup3.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
-      <h2>Step 3: Switch to your Ubuntu laptop</h2>
+      <h2>Step 3: Switch to your Ubuntu Laptop</h2>
       <div>On your freshly installed Ubuntu laptop open Firefox and type the following URL into the address bar to continue to Step 4.</div>
-      <cold>warm.yeticold.com</cold>
+      <code>warm.yeticold.com</code>
     </div>
 </template>
