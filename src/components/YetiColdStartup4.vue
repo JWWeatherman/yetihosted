bd9a620e1c07839f586ff01153be2cbe38cfa2d3
JW Weatherman <jw@jwweatherman.com>
2019-12-01 11:19:40 -0500

    Text

--- src/components/YetiColdStartup4.vue
+++ src/components/YetiColdStartup4.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
-      <h2>Step 4: Download Yeti to your ONLINE laptop</h2>
+      <h2>Step 4: Download Yeti to your Online laptop</h2>
       <ol>
-          <li>Click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Terminal</b>.</li>
+          <li>On your <b>Online</b> laptop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Terminal</b>.</li>
           <li>Click <b>Copy</b> to copy the text below this line.</li>
           <div style="width:75%;" class="input-group mb-3 mt-3">
             <input v-on:click="copy1" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy1" aria-describedby="inputGroup-sizing-default" value="sudo apt-get install git">
@@ -19,7 +19,7 @@
               <span v-on:click="copy2" class="btn btn-secondary" id="basic-addon2">Copy</span>
             </div>
           </div>
-          <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>.</li>
+          <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>. If you see an error that the directory already exists ignore it as this just means Yeti is already installed on this laptop.</li>
           <li>Click <b>Next</b> to continue.</li>
       </ol>          
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-primary" type="submit" id="next" value="Next">
