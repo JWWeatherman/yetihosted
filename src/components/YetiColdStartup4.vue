7c444aa241814ec6743be73ace89da81cf726795
willweatherman <will@mathbot.com>
2019-12-03 14:03:09 -0500

    changed commands

--- src/components/YetiColdStartup4.vue
+++ src/components/YetiColdStartup4.vue
@@ -5,20 +5,11 @@
           <li>On your <b>Online</b> laptop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Terminal</b>.</li>
           <li>Click <b>Copy</b> to copy the text below this line.</li>
           <div style="width:75%;" class="input-group mb-3 mt-3">
-            <input v-on:click="copy1" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy1" aria-describedby="inputGroup-sizing-default" value="sudo apt-get install git">
+          <input v-on:click="copy2" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy2" aria-describedby="inputGroup-sizing-default" value="sudo apt-get update; sudo apt-get install git; git clone https://github.com/jwweatherman/yeticold.git ~/yeticold">
             <div class="input-group-append">
               <span v-on:click="copy1" class="btn btn-secondary" id="basic-addon2">Copy</span>
             </div>
-          </div>
-          <li>In the Terminal window right click and select <b>Paste</b> and then click <b>Enter</b>.</li>
-          <li>Enter your password and then click <b>Enter</b>.</li>
-          <li>Click <b>Copy</b> to copy the text below this line.</li>
-          <div style="width:75%;" class="input-group mb-3 mt-3">
-            <input v-on:click="copy2" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy2" aria-describedby="inputGroup-sizing-default" value="git clone https://github.com/jwweatherman/yeticold.git ~/yeticold">
-            <div class="input-group-append">
-              <span v-on:click="copy2" class="btn btn-secondary" id="basic-addon2">Copy</span>
-            </div>
-          </div>
+          </div>          
           <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>. If you see an error that the directory already exists ignore it as this just means Yeti is already installed on this laptop.</li>
           <li>Click <b>Next</b> to continue.</li>
       </ol>          
--- src/components/YetiWarmStartup4.vue
+++ src/components/YetiWarmStartup4.vue
@@ -2,26 +2,17 @@
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 4: Download Yeti to your laptop</h2>
       <ol>
-          <li>Click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Terminal</b>.</li>
-          <li>Click "copy" to copy the text below this line.</li>
+          <li>On your <b>Online</b> laptop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Terminal</b>.</li>
+          <li>Click <b>Copy</b> to copy the text below this line.</li>
           <div style="width:75%;" class="input-group mb-3 mt-3">
-            <input v-on:click="copy1" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy1" aria-describedby="inputGroup-sizing-default" value="sudo apt-get install git">
+          <input v-on:click="copy2" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy2" aria-describedby="inputGroup-sizing-default" value="sudo apt-get update; sudo apt-get install git; git clone https://github.com/jwweatherman/yeticold.git ~/yeticold">
             <div class="input-group-append">
               <span v-on:click="copy1" class="btn btn-secondary" id="basic-addon2">Copy</span>
             </div>
-          </div>
-          <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>.</li>
-          <li>Enter your password and then click <b>Enter</b>.</li>
-          <li>Click "copy" to copy the text below this line.</li>
-          <div style="width:75%;" class="input-group mb-3 mt-3">
-            <input v-on:click="copy2" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy2" aria-describedby="inputGroup-sizing-default" value="git clone https://github.com/jwweatherman/yeticold.git ~/yeticold">
-            <div class="input-group-append">
-              <span v-on:click="copy2" class="btn btn-secondary" id="basic-addon2">Copy</span>
-            </div>
-          </div>
-          <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>.</li>
+          </div>          
+          <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>. If you see an error that the directory already exists ignore it as this just means Yeti is already installed on this laptop.</li>
           <li>Click <b>Next</b> to continue.</li>
-      </ol>          
+      </ol>      
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
 </template>
