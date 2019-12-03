af95a900ba9d0dee0f13b9248e61303717814c6c
willweatherman <will@mathbot.com>
2019-12-03 14:00:43 -0500

    changed commands

--- src/components/YetiHotStartup4.vue
+++ src/components/YetiHotStartup4.vue
@@ -2,25 +2,16 @@
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 4: Download Yeti to your ONLINE laptop</h2>
       <ol>
-          <li>Click on the 9 dots in the bottom left corner of your Ubuntu desktop and then click Terminal.</li>
-          <li>Click "copy" to copy the text below this line.</li>
-          <div style="width:75%;" class="input-group mb-3 mt-3">
-            <input v-on:click="copy1" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy1" aria-describedby="inputGroup-sizing-default" value="sudo apt-get install git">
-            <div class="input-group-append">
-              <span v-on:click="copy1" class="btn btn-secondary" id="basic-addon2">Copy</span>
-            </div>
+        <li>Click on the 9 dots in the bottom left corner of your Ubuntu desktop and then click Terminal.</li>
+        <li>Click "copy" to copy the text below this line.</li>
+        <div style="width:75%;" class="input-group mb-3 mt-3">
+          <input v-on:click="copy2" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy2" aria-describedby="inputGroup-sizing-default" value="sudo apt-get update; sudo apt-get install git; git clone https://github.com/jwweatherman/yeticold.git ~/yeticold">
+          <div class="input-group-append">
+            <span v-on:click="copy2" class="btn btn-secondary" id="basic-addon2">Copy</span>
           </div>
-          <li>In the terminal window right click and select Paste and then click Enter.</li>
-          <li>Enter your password and then click Enter.</li>
-          <li>Click "copy" to copy the text below this line.</li>
-          <div style="width:75%;" class="input-group mb-3 mt-3">
-            <input v-on:click="copy2" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy2" aria-describedby="inputGroup-sizing-default" value="git clone https://github.com/jwweatherman/yeticold.git ~/yeticold">
-            <div class="input-group-append">
-              <span v-on:click="copy2" class="btn btn-secondary" id="basic-addon2">Copy</span>
-            </div>
-          </div>
-          <li>In the terminal window right click and select Paste and then click Enter.</li>
-          <li>Click next to continue.</li>
+        </div>
+        <li>In the terminal window right click and select Paste and then click Enter.</li>
+        <li>Click next to continue.</li>
       </ol>          
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-danger" type="submit" id="next" value="Next">
     </div>
