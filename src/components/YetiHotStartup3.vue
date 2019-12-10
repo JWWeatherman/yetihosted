b526105877c7daf522899f8eaa1669614733d679
willweatherman <will@mathbot.com>
2019-12-10 11:18:06 -0500

    fixed styling on step 3 Hot

--- src/components/YetiHotStartup3.vue
+++ src/components/YetiHotStartup3.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container" style="margin-top: 3rem;">
-      <h2>Step 3: Switch to your Ubuntu laptop</h2>
-      <div>On your freshly installed Ubuntu laptop go to "hot.yeticold.com" to continue to Step 4.</div>
+      <h2>Step 3: Switch to your Ubuntu Laptop</h2>
+      <div>On your freshly installed Ubuntu laptop open Firefox and type the following URL into the address bar to continue to Step 4.</div>
+      <code>hot.yeticold.com</code>
     </div>
 </template>
