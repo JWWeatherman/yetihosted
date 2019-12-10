f17380619f85157117a3afef3be6647435dd6c71
willweatherman <will@mathbot.com>
2019-12-10 11:38:33 -0500

    changed navbars on yetihosted

--- src/components/navbarhot.vue
+++ src/components/navbarhot.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="form-row navbar navbar-light bg-danger">
-        <img v-on:click="Home" src="@/assets/logo.png" width="50" height="50" class="align-top" alt="">
+        <img src="@/assets/logo.png" width="50" height="50" class="align-top" alt="">
         <h3 style="color:black;margin:0;">Yeti Bitcoin Cold Storage</h3>
     </nav>
 </template>
