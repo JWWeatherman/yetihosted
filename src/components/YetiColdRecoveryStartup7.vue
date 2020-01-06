3873fcfcba003607f6755bf1aba96ff6e91349bf
willweatherman <will@mathbot.com>
2020-01-06 16:10:15 -0500

    bug fix

--- src/components/YetiColdRecoveryStartup7.vue
+++ src/components/YetiColdRecoveryStartup7.vue
@@ -2,7 +2,7 @@
   <div>
     <navbarcold></navbarcold>
     <div class="container" style="margin-top: 3rem;">
-      <h2>Step 2: Start Yeticold on your <b>Disconnected</b> laptop.</h2>
+      <h2>Step 2: Start Yeti Cold Recovery on your <b>Disconnected</b> laptop.</h2>
       <ol>
           <li>On your <b>Online</b> laptop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Terminal</b>.</li>
           <li>Click <b>Copy</b> to copy the text below this line.</li>
@@ -15,14 +15,14 @@
           <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>.</li>
           <li>Click <b>Copy</b> to copy the text below this line.</li> 
           <div style="width:75%;" class="input-group mb-3 mt-3">
-            <input v-on:click="copy1" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy1" aria-describedby="inputGroup-sizing-default" value="python3 ~/yeticold/scripts/rec.py">
+            <input v-on:click="copy2" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy2" aria-describedby="inputGroup-sizing-default" value="python3 ~/yeticold/scripts/rec.py">
             <div class="input-group-append">
-              <span v-on:click="copy1" class="btn btn-secondary" id="basic-addon2">Copy</span>
+              <span v-on:click="copy2" class="btn btn-secondary" id="basic-addon2">Copy</span>
             </div>
           </div>
           <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>.</li>
           <li>When a new browser window is opened click <b>Try Again</b>.</li>
-          <li>Once Step 3 is shown, close this tab to continue with Step 3 in the other tab.</li>
+          <li>Once Step 9 is shown, close this tab to continue with Step 9 in the other tab.</li>
       </ol> 
     </div>
   </div>
@@ -34,6 +34,12 @@ export default {
   methods: {
     copy1 () {
       let copyText = document.getElementById("copy1")
+      copyText.select()
+      copyText.setSelectionRange(0, 99999)
+      document.execCommand("copy")
+    },
+    copy2 () {
+      let copyText = document.getElementById("copy2")
       copyText.select()
       copyText.setSelectionRange(0, 99999)
       document.execCommand("copy")
