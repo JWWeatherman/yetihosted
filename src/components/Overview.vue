bd934b7399d63441879ae7d16043c597fff5cb79
JW Weatherman <jw@jwweatherman.com>
2020-02-16 15:15:19 -0500

    Text

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -5,21 +5,21 @@
       <h2></h2>
       <div class="hero-unit">
           <h1>Yeti Cold Storage</h1>
-          <p>Yeti Cold Storage is for storing more than $50,000 USD worth of bitcoin. It is a 3 of 7 HD multisig setup where private keys are never on an internet connected device.</p>
+          <p>Yeti Cold Storage is recommended for more than $50,000 USD worth of bitcoin. It is a 3 of 7 HD multisig setup where private keys are never on an internet connected device.</p>
           <p>
             <input v-on:click="Cold" style="margin-bottom:50px;" class="btn btn-primary btn-large" type="submit" id="next" value="Yeti Cold">
           </p>
       </div>
       <div class="hero-unit">
           <h1>Yeti Warm Storage</h1>
-          <p>Yeti Warm Storage is for storing between $5,000 and $50,000 USD worth of bitcoin. It is a 3 of 7 HD multisig setup that requires only a single computer to setup.</p>
+          <p>Yeti Warm Storage is recommended for storing between $5,000 and $50,000 USD worth of bitcoin. It is a 3 of 7 HD multisig setup that requires only a single computer to setup.</p>
           <p>
             <input v-on:click="Warm" style="margin-bottom:50px;" class="btn btn-warning btn-large" type="submit" id="next" value="Yeti Warm">
           </p>
       </div>
       <div class="hero-unit">
           <h1>Yeti Hot Storage</h1>
-          <p>Yeti Hot Storage is for storing under $5,000 USD worth of bitcoin. It is very easy to backup and restore and requires only a single computer to setup.</p>
+          <p>Yeti Hot Storage is recommended for storing under $5,000 USD worth of bitcoin. It is very easy to backup and restore and requires only a single computer to setup.</p>
           <p>
             <input v-on:click="Hot" style="margin-bottom:50px;" class="btn btn-danger btn-large" type="submit" id="next" value="Yeti Hot">
           </p>
