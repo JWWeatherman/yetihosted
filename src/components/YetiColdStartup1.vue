20816ddfd43139e485d01e188b0d368b60e64212
willweatherman <will@mathbot.com>
2020-12-23 16:27:29 -0500

    refactor yeti

--- src/components/YetiColdStartup1.vue
+++ src/components/YetiColdStartup1.vue
@@ -5,11 +5,11 @@
       <h2>Step 1: Gather Required Equipment</h2>
       <p>Gather the following items and then click <b>Next</b>.</p>
         <ul>
-            <li>Two laptops that can run Ubuntu and Bitcoin Core. At the time of this writing almost any laptop will work but if one of your laptops has an SSD type hard some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD.</li>
-            <li>A CD drive to wright to the CDs. Some laptops come with one built in.</li>
+            <li>Two laptops that can run Ubuntu and Bitcoin Core. At the time of this writing almost any laptop will work but if one of your laptops has an SSD type hard drive some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD.</li>
+            <li>A CD drive to write to the CDs. Some laptops come with one built in.</li>
             <li>Fifteen CD disks.</li>
             <li>A printer and printer paper.</li>
-            <li>Two small USB thumb drives. These will cost about $3 each and will be used to store your Bitcoin seeds along with a paper copy.</li>
+            <li>Two USB thumb drives. These will cost about $3 each and will be used to store your Bitcoin seeds along with a paper copy.</li>
         </ul>
       <input v-on:click="click" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
--- src/components/YetiWarmStartup1.vue
+++ src/components/YetiWarmStartup1.vue
@@ -5,8 +5,8 @@
       <h2>Step 1: Gather Required Equipment</h2>
       <p>Gather the following items and then click next.</p>
         <ul>
-            <li>A laptops that can run Ubuntu and Bitcoin Core. At the time of this writing almost any laptop will work but if your laptop has an SSD type hard some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD.</li>
-            <li>1 small USB thumb drives. These will cost about $3 each and will be used to store your Bitcoin seeds along with a paper copy.</li>
+            <li>A laptop that can run Ubuntu and Bitcoin Core. At the time of this writing almost any laptop will work but if your laptop has an SSD type hard some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD.</li>
+            <li>One USB thumb drive. It will cost about $3 and will be used to Create a Bootable USB drive in the next step.</li>
             <li>A printer and printer paper.</li>
         </ul>
       <input v-on:click="click" class="btn btn-warning" type="submit" id="next" value="Next">
