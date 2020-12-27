3dfa38859985f02f064fdb93508f9cfa5e8bb364
JW Weatherman <jw@jwweatherman.com>
2020-12-26 19:51:02 -0500

    Better installiton instruction text.

--- src/components/YetiColdStartup2.vue
+++ src/components/YetiColdStartup2.vue
@@ -3,17 +3,14 @@
     <navbarcold></navbarcold>
     <div class="container" style="margin-top: 3rem;">
       <h2>Step 2: Install Ubuntu and Label Laptops</h2>
-      <ol>
-        <li>Install Ubuntu on both laptops following these rules.</li>
+      <p>Install Ubuntu on both laptops following these rules.</p>
         <ul>
             <li>Use the Long Term Support (LTS) Version of Ubuntu. This is currently 20.04 and is at the top of the download page.</li>
-            <li>Delete and erase all existing partitions using the Disk Utility that you can access by selecting "Try Ubuntu" before installing Ubuntu.</li>
+            <li>Erase all existing data on the hard drive before installing.</li>
             <li>Use only trustworthy guides such as this one: <a href="https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0">Install Ubuntu</a>.</li>
+            <li>Install security updates using Software Updater.</li>
         </ul>
-        <li>After you see the desktop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Software Updater</b>.</li>
-        <li>Click <b>Install Now</b>.</li>
-        <li>When it has finished installing the updates click <b>Next</b> on this page.</li>
-      </ol>
+        <p>When you have completed this for both laptops click <b>Next</b> to continue.</p>
       <input v-on:click="click" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
   </div>
