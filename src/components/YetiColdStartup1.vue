f5e900bb3425da9fac3329a84bad3d1086856b52
JW Weatherman <jw@jwweatherman.com>
2020-12-26 19:44:06 -0500

    Typo

--- src/components/YetiColdStartup1.vue
+++ src/components/YetiColdStartup1.vue
@@ -5,7 +5,7 @@
       <h2>Step 1: Gather and Label Required Equipment</h2>
       <p>Gather the following items and then click <b>Next</b>.</p>
         <ul>
-            <li>2 laptops that can run Ubuntu and Bitcoin Core. Almost any laptop made for Windows will work, but if one of your laptops has an SSD type hard drive some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD. Label one laptop "Primary" and the other "Secondary."</li>
+            <li>2 laptops that can run Ubuntu and Bitcoin Core. Almost any laptop made for Windows will work, but if your laptops have an SSD type hard drive some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD. Label one laptop "Primary" and the other "Secondary."</li>
             <li>A CD-ROM drive to write to the CDs. Most laptops come with one built in, but you can also buy an external CD-ROM drive for about $15 on Amazon.</li>
             <li>14 blank CD-ROM disks. Label 7 of the blank disks "Descriptor" and the other 7 "Seed 1" "Seed 2" "Seed 3" through "Seed 7."</li>
             <li>A printer and printer paper.</li>
