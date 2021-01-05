c7da3760438956c30c9b201b926db20d5b5d55a3
Robert Spigler <RobertSpigler@ProtonMail.ch>
2021-01-05 16:39:25 -0500

    Update YetiHotStartup2.vue
    Re-order steps/clarification

--- src/components/YetiHotStartup2.vue
+++ src/components/YetiHotStartup2.vue
@@ -7,9 +7,10 @@
         <li>Install Ubuntu on both laptops following these rules.</li>
         <ul>
             <li>Use the Long Term Support (LTS) Version of Ubuntu. This is currently 20.04 and is at the top of the download page. We can not guarantee that Yeticold will work with any other Ubuntu version.</li>
-            <li>Delete and erase all existing partitions using the Disk Utility that you can access by selecting "Try Ubuntu" before installing Ubuntu.</li>
-            <li>Use one of your USB drives to create a Bootable Ubuntu drive.</li>
             <li>Use only trustworthy guides such as this one: <a href="https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0">Install Ubuntu</a>.</li>
+            <li>Use one of your USB drives to create a Bootable Ubuntu drive.</li>
+            <li>Select "Try Ubuntu", and then use the Disk Utility to delete and erase all existing partitions.</li>
+            <li>Create a new partition consisting of the entire device, and then install Ubuntu.</li>
         </ul>
         <li>After you see the desktop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Software Updater</b>.</li>
         <li>Click <b>Install Now</b>.</li>
