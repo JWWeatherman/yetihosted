34d6fe4cc34f60b571e2bdb927142dc5666064c4
willweatherman <will@mathbot.com>
2021-01-14 09:18:42 -0500

    refactor yeti

--- src/components/navbarhot.vue
+++ src/components/navbarhot.vue
@@ -1,5 +1,6 @@
 <template>
     <nav class="form-row navbar navbar-light bg-danger">
+    	<a href="https://yeticold.com">
         <img src="@/assets/logo.png" width="50" height="50" class="align-top" alt="">
         <h3 style="color:white;margin:0;">Yeti Level 1 Wallet</h3>
     </nav>
