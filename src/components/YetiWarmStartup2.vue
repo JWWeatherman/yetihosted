3779eb9aec2d415e4f5ba3a4d18336486624ea82
willweatherman <will@mathbot.com>
2021-01-14 09:26:26 -0500

    refactor yeti

--- src/components/YetiWarmStartup2.vue
+++ src/components/YetiWarmStartup2.vue
@@ -7,8 +7,8 @@
         <li>Install Ubuntu on your laptop following these steps and then click <b>Next</b> to continue.</li>
         <ul>
             <li>Use the Long Term Support (LTS) Version of Ubuntu. This is currently 20.04 and is at the top of the download page.</li>
-            <li>Delete and erase all existing partitions using the Disk Utility that you can access by selecting "Try Ubuntu" before installing Ubuntu.</li>
             <li>Use only trustworthy guides such as this one: <a href="https://tutorials.ubuntu.com/tutorial/tutorial-install-ubuntu-desktop#0">Install Ubuntu</a>.</li>
+            <li>Delete and erase all existing partitions using the Disk Utility that you can access by selecting "Try Ubuntu" before installing Ubuntu.</li>
         </ul>
         <li>After you see the desktop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Software Updater</b>.</li>
         <li>Click <b>Install Now</b>.</li>
