8ea8e503386d8e9a615f053e40ffe5cee46e48f8
willweatherman <will@mathbot.com>
2021-01-14 09:05:38 -0500

    refactor yeti

--- src/components/YetiColdTest3.vue
+++ src/components/YetiColdTest3.vue
@@ -6,13 +6,13 @@
       <p>Now we are going to encrypt and erase the hard drive that stores your private keys on your <b>Secondary</b> laptop.</p>
       <ol>
         <li>Power off your <b>Secondary laptop</b>.</li>
-        <li>Insert the usb drive you used to install ubuntu.</li>
-        <li>Boot up your laptop and click <b>Try ubuntu</b>.</li>
-        <li>Insert your "Transfer" usb and copy the erase.txt file to the "Documents" folder.</li>
+        <li>Insert the usb drive you used to install Ubuntu.</li>
+        <li>Boot up your laptop and click <b>Try Ubuntu</b>.</li>
+        <li>Insert your "Transfer" usb and copy the "erase.txt" file to the "Documents" folder.</li>
         <li>Click on the <b>nine dots</b> in the bottom left corner and open "Terminal".</li>
-        <li>Open the erase.txt file stored in the "Documents" folder and copy the first command.</li>
+        <li>Open the "erase.txt" file stored in the "Documents" folder and copy the first command.</li>
         <li>Paste the text in the "Terminal" and hit <b>Enter</b>.</li>
-        <li>Confirm that the hard drive is "not frozen" See image below. If it's "frozen" contatct support at "yeticold.slack.com".</li>
+        <li>Confirm that the hard drive is "not frozen" See image below. If it's "frozen" contact support at "yeticold.slack.com".</li>
         <div class="card mb-3" style="width: 50%;">
           <img class="card-img-top" src="@/assets/terminal1.png" alt="Card image cap">
         </div>
