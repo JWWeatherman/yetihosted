71bd184044ecbe6c583a53d294b87d7b0581f43c
willweatherman <will@mathbot.com>
2021-01-20 13:38:21 -0500

    Fixed favicon

--- src/components/YCTest3.vue
+++ src/components/YCTest3.vue
@@ -22,8 +22,7 @@
           <img class="card-img-top" src="@/assets/terminal2.png" alt="Card image cap">
         </div>
         <li>Open the "erase.txt" file stored in the "Documents" folder and copy the third command.</li>
-        <li>Paste the text in the terminal and hit <b>Enter</b>.</li>
-        <li>After a few minutes you should see the following output:</li>
+        <li>Paste the text in the terminal and hit <b>Enter</b>. After a few minutes you should see the following output:</li>
         <div class="card mb-3" style="width: 50%;">
           <img class="card-img-top" src="@/assets/terminal3.png" alt="Card image cap">
         </div>
