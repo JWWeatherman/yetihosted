65f22ab927f0d2f84930a671138e97c093d62105
willweatherman <will@mathbot.com>
2021-01-20 11:57:06 -0500

    Fixed cwg

--- src/components/navbarwarm.vue
+++ src/components/navbarwarm.vue
@@ -1,6 +1,5 @@
 <template>
     <nav class="form-row navbar navbar-light bg-warning">
-    	<a href="https://yeticold.com"></a>
         <img src="@/assets/logo.png" width="50" height="50" class="align-top" alt="">
         <h3 style="color:black;margin:0;">Yeti Level 2 Wallet</h3>
     </nav>
