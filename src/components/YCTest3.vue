3318a3c0fc4d39a9208704b3a3314c4d1b7a31c1
willweatherman <will@mathbot.com>
2021-01-20 13:35:33 -0500

    Fixed favicon

--- src/components/YCTest3.vue
+++ src/components/YCTest3.vue
@@ -12,18 +12,18 @@
         <li>Click on the <b>nine dots</b> in the bottom left corner and open <b>Terminal</b>.</li>
         <li>Open the "erase.txt" file stored in the "Documents" folder and copy the first command.</li>
         <li>Paste the text in the terminal and hit <b>Enter</b>.</li>
-        <li>Confirm that the hard drive is "not frozen" See image below. If it's "frozen" contact support at "yeticold.slack.com".</li>
+        <li>Confirm that the hard drive is "not frozen". If it's "frozen" contact support at "yeticold.slack.com".</li>
         <div class="card mb-3" style="width: 50%;">
           <img class="card-img-top" src="@/assets/terminal1.png" alt="Card image cap">
         </div>
-        <li>Open the erase.txt file stored in the "Documents" folder and copy the second command.</li>
-        <li>Paste the text in the "Terminal" and hit <b>Enter</b>.</li>
+        <li>Open the "erase.txt" file stored in the "Documents" folder and copy the second command.</li>
+        <li>Paste the text in the terminal and hit <b>Enter</b>.</li>
         <li>You should see an output like below.</li>
         <div class="card mb-3" style="width: 50%;">
           <img class="card-img-top" src="@/assets/terminal2.png" alt="Card image cap">
         </div>
-        <li>Open the erase.txt file stored in the "Documents" folder and copy the third command.</li>
-        <li>Paste the text in the "Terminal" and hit <b>Enter</b>.</li>
+        <li>Open the "erase.txt" file stored in the "Documents" folder and copy the third command.</li>
+        <li>Paste the text in the terminal and hit <b>Enter</b>.</li>
         <li>After a few minutes you should see an output like below.</li>
         <div class="card mb-3" style="width: 50%;">
           <img class="card-img-top" src="@/assets/terminal3.png" alt="Card image cap">
