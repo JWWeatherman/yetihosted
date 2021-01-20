40950ee0a53709bdbd9707aade0ef5185389e59d
willweatherman <will@mathbot.com>
2021-01-20 13:32:37 -0500

    Fixed favicon

--- src/components/YCTest3.vue
+++ src/components/YCTest3.vue
@@ -2,16 +2,16 @@
   <div>
     <navbarcold></navbarcold>
     <div class="container" style="margin-top: 3rem;">
-      <h2>Step 34: Erase your hard drive on your Secondary Laptop</h2>
-      <p>Now we are going to encrypt and erase the hard drive that stores your private keys on your <b>Secondary</b> laptop.</p>
+      <h2>Step 34: Erase your Laptops</h2>
+      <p>Erase your Laptops to ensure your bitcoin can only be spent using your seed packets.</p>
       <ol>
         <li>Power off your <b>Secondary laptop</b>.</li>
         <li>Insert the usb drive you used to install Ubuntu.</li>
         <li>Boot up your laptop and click <b>Try Ubuntu</b>.</li>
         <li>Insert your "Transfer" usb and copy the "erase.txt" file to the "Documents" folder.</li>
-        <li>Click on the <b>nine dots</b> in the bottom left corner and open "Terminal".</li>
+        <li>Click on the <b>nine dots</b> in the bottom left corner and open <b>Terminal</b>.</li>
         <li>Open the "erase.txt" file stored in the "Documents" folder and copy the first command.</li>
-        <li>Paste the text in the "Terminal" and hit <b>Enter</b>.</li>
+        <li>Paste the text in the terminal and hit <b>Enter</b>.</li>
         <li>Confirm that the hard drive is "not frozen" See image below. If it's "frozen" contact support at "yeticold.slack.com".</li>
         <div class="card mb-3" style="width: 50%;">
           <img class="card-img-top" src="@/assets/terminal1.png" alt="Card image cap">
