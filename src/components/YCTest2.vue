5fb61930db6e780fe6bbff37d8f59c8541081e49
willweatherman <will@mathbot.com>
2021-01-20 13:25:08 -0500

    Fixed favicon

--- src/components/YCTest2.vue
+++ src/components/YCTest2.vue
@@ -5,8 +5,8 @@
       <h2>Step 33: Restore your Wallet</h2>
       <p>Restore your wallet with two sets of CDs to make sure you can recover your wallet with any three.</p>
       <ol>
-        <li>Set aside 3 "Seed" CDs labeled "Seed 1", "Seed 2", and "Seed 3".</li>
-        <li>Restart yeti on you <b>Primary</b> by clicking on the <b>nine dots</b> in the bottom left corner and open <b>LevelThree</b>.</li>
+        <li>Set aside three Seed CDs labeled "Seed 1," "Seed 2," and "Seed 3."</li>
+        <li>Restart Yeti on you <b>Primary</b> by clicking on the <b>nine dots</b> in the bottom left corner and click the <b>LevelThree</b> icon.</li>
         <li>Click <b>Recover</b> and follow all the steps starting at step 5. When it asks you to import your seeds use the three CDs you set aside.</li>
         <li>When you see the guide for sending and receiving follow all the steps to confirm you successfully restored your wallet.</li>
         <li>Repeat steps 2 through 4 with the "Seed" CDs labeled "Seed4", "Seed5", and "Seed6".</li>
