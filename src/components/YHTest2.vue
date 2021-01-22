4ffb2d220847d300d66d511fc2ceaa12def2b331
JW Weatherman <jw@jwweatherman.com>
2021-01-22 15:03:07 -0500

    Minor text changes.

--- src/components/YHTest2.vue
+++ src/components/YHTest2.vue
@@ -3,13 +3,11 @@
     <navbarhot></navbarhot>
     <div class="container" style="margin-top: 3rem;">
       <h2 style="margin-bottom:35px;">Step 16: Restore your Wallet</h2>
-      <p>First we are going to restore your wallet with one of your CDs to make sure you can recover your wallet.</p>
+      <p>Restore your wallet with one of your CDs to make sure you can recover your wallet.</p>
       <ol>
-        <li>Restart yeti on you laptop by clicking on the <b>nine dots</b> in the bottom left corner and open <b>LevelOne</b>.</li>
-        <li>Click <b>Recover</b> and follow all the steps starting at step 5. When it asks you to import your seeds use the any of your "seed" CDs.</li>
-        <li>When you see the step to go to <code>hwg.yeticold.com</code> ignore that and instead follow the bellow guide to make sure your wallet is still working.</li>
-        <li>After you have restored and tested your wallet you can now send and receive safely.</li>
-        <li>After you are finished and are ready to erase your laptop and private keys click <b>Next</b>.</li>
+        <li>Restart Yeti on your laptop by clicking on the <b>nine dots</b> in the bottom left corner and open <b>Level One</b>.</li>
+        <li>Click <b>Recover</b> and follow the steps until you have tested sending and receiving a small amount of bitcoin using your freshly restored wallet.</li>
+        <li>You have thoroughly tested your Yeti Wallet. Deposit the funds you wish to secure in your Yeti Wallet and then click <b>Next</b>to continue.</li>
       </ol>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-danger" type="submit" id="next" value="Next">
     </div>
--- src/components/YWTest2.vue
+++ src/components/YWTest2.vue
@@ -5,14 +5,12 @@
        <h2 style="margin-bottom:35px;">Step 16: Restore your Wallet</h2>
       <p>Next we are going to restore your wallet with two sets of CDs to make sure you can recover your wallet with any three</p>
       <ol>
-        <li>Set aside 3 "Seed" CDs labeled "Seed 1", "Seed 2", and "Seed 3".</li>
-        <li>Restart yeti on you <b>Primary</b> by clicking on the <b>nine dots</b> in the bottom left corner and open <b>LevelThree</b>.</li>
-        <li>Click <b>Recover</b> and follow all the steps starting at step 5. When it asks you to import your seeds use the three CDs you set aside.</li>
-        <li>When you see the guide for sending and receiving follow all the steps to confirm you successfully restored your wallet.</li>
-        <li>Repeat steps 2 through 4 with the "Seed" CDs labeled "Seed4", "Seed5", and "Seed6".</li>
-        <li>After you have restored and tested your wallet twice with 6 different seeds your wallet is tested and you can now send and receive safely.</li>
-        <li>After you are finished and are ready to erase your laptop and private keys click <b>Next</b>.</li>
-      </ol>
+         <li>Set aside three Seed CDs labeled "Seed 1," "Seed 2," and "Seed 3."</li>
+         <li>Restart Yeti on you <b>Primary</b> by clicking on the <b>nine dots</b> in the bottom left corner and click the <b>Level Two</b> icon.</li>
+         <li>Click <b>Recover</b> and follow all the steps until you have tested sending and receiving a small amount of    bitcoin using these seeds. Be sure that when it asks you to import your seeds use the three CDs you set aside.</li>
+         <li>After you have sent and received a small amount, but <b>before you erase your laptop</b>, repeat steps 2       through 4 with the CDs labeled "Seed 4," "Seed 5", and "Seed 6."</li>
+         <li>You have thoroughly tested your Yeti Wallet. Deposit the funds you wish to secure in your Yeti Wallet and      then click <b>Next</b>to continue.</li>
+       </ol>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-warning" type="submit" id="next" value="Next">
     </div>
   </div>
