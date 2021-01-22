fb8c36301a1fa79955d43053826291cc48be6641
JW Weatherman <jw@jwweatherman.com>
2021-01-22 14:40:47 -0500

    Improved instruction text.

--- src/components/YCTest2.vue
+++ src/components/YCTest2.vue
@@ -6,11 +6,10 @@
       <p>Restore your wallet with two sets of CDs to make sure you can recover your wallet with any three.</p>
       <ol>
         <li>Set aside three Seed CDs labeled "Seed 1," "Seed 2," and "Seed 3."</li>
-        <li>Restart Yeti on you <b>Primary</b> by clicking on the <b>nine dots</b> in the bottom left corner and click the <b>LevelThree</b> icon.</li>
-        <li>Click <b>Recover</b> and follow all the steps starting at step 5. When it asks you to import your seeds use the three CDs you set aside.</li>
-        <li>After you have received and sent a small amount before you erase your laptop repeat steps 2 through 4 with the "Seed" CDs labeled "Seed4", "Seed5", and "Seed6".</li>
-        <li>After you have restored and tested your wallet twice with 6 different seeds your wallet is tested and you can now send and receive safely.</li>
-        <li>After you are finished and are ready to erase your laptop and private keys click <b>Next</b>.</li>
+        <li>Restart Yeti on you <b>Primary</b> by clicking on the <b>nine dots</b> in the bottom left corner and click the <b>Level Three</b> icon.</li>
+        <li>Click <b>Recover</b> and follow all the steps until you have tested sending and receiving a small amount of bitcoin using these seeds. Be sure that when it asks you to import your seeds use the three CDs you set aside.</li>
+        <li>After you have send and received a small amount, but before you erase your laptop, repeat steps 2 through 4 with the CDs labeled "Seed 4," "Seed 5", and "Seed 6."</li>
+        <li>You have thoroughly tested your Yeti Wallet. Deposit the funds you wish to secure in your Yeti Wallet and then click <b>Next</b>to continue.</li>
       </ol>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-primary" type="submit" id="next" value="Next">
     </div>
