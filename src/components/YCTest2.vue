6bf0ee8d6ed1b64a9d08a41d6e829cf39c192d27
JW Weatherman <jw@jwweatherman.com>
2021-01-22 14:50:44 -0500

    bolded warning

--- src/components/YCTest2.vue
+++ src/components/YCTest2.vue
@@ -8,7 +8,7 @@
         <li>Set aside three Seed CDs labeled "Seed 1," "Seed 2," and "Seed 3."</li>
         <li>Restart Yeti on you <b>Primary</b> by clicking on the <b>nine dots</b> in the bottom left corner and click the <b>Level Three</b> icon.</li>
         <li>Click <b>Recover</b> and follow all the steps until you have tested sending and receiving a small amount of bitcoin using these seeds. Be sure that when it asks you to import your seeds use the three CDs you set aside.</li>
-        <li>After you have send and received a small amount, but before you erase your laptop, repeat steps 2 through 4 with the CDs labeled "Seed 4," "Seed 5", and "Seed 6."</li>
+        <li>After you have send and received a small amount, but <b>before you erase your laptop</b>, repeat steps 2 through 4 with the CDs labeled "Seed 4," "Seed 5", and "Seed 6."</li>
         <li>You have thoroughly tested your Yeti Wallet. Deposit the funds you wish to secure in your Yeti Wallet and then click <b>Next</b>to continue.</li>
       </ol>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-primary" type="submit" id="next" value="Next">
