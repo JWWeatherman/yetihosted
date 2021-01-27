167d1923aa5f93655ddf1126aacbb2c4a476851a
JW Weatherman <jw@jwweatherman.com>
2021-01-27 17:27:34 -0500

    Deposit instructions overlap.

--- src/components/YHTest2.vue
+++ src/components/YHTest2.vue
@@ -6,8 +6,8 @@
       <p>Restore your wallet with one of your CDs to make sure you can recover your wallet.</p>
       <ol>
         <li>Restart Yeti on your laptop by clicking on the <b>nine dots</b> in the bottom left corner and open <b>Level One</b>.</li>
-        <li>Click <b>Recover</b> and follow the steps until you have tested sending and receiving a small amount of bitcoin using your freshly restored wallet.</li>
-        <li>You have thoroughly tested your Yeti Wallet. Deposit the funds you wish to secure in your Yeti Wallet and then click <b>Next</b> to continue.</li>
+        <li>Click <b>Recover Wallet</b> and follow the steps until you have tested sending and receiving a small amount of bitcoin using your freshly restored wallet.</li>
+        <li>When you have finished testing your restored wallet click <b>Next</b> to continue.</li>
       </ol>
       <input v-on:click="click" style="margin-top:35px;" class="btn btn-danger" type="submit" id="next" value="Next">
     </div>
