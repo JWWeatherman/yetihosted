b0115b2fb3265aeae5a088f62ad54b4c62d03b0e
JW Weatherman <jw@jwweatherman.com>
2021-03-22 13:44:04 -0400

    Instruction for labeling envelopes.

--- src/components/YCSetup1.vue
+++ src/components/YCSetup1.vue
@@ -10,7 +10,7 @@
             <li>14 blank CD-R disks. Label 7 of the blank disks "Descriptor" and the other 7 "Seed 1" "Seed 2" "Seed 3" through "Seed 7."</li>
             <li>A printer and printer paper.</li>
             <li>2 USB thumb drives. These will cost about $3 each and will be used to install Ubuntu and to transfer transactions between your laptops. Label one "Ubuntu" and the other "Transfer."</li>
-            <li>7 full page size envelopes.</li>
+            <li>7 full page size envelopes. Label them "Do not open. Last Will and Testament. Hand deliver ONLY to 'Your Name' or executor." Also place the number 1 through 7 on each envelope to help you keep track in future steps.</li>
         </ul>
       <input v-on:click="click" class="btn btn-primary" type="submit" id="next" value="Next">
       <p></p>
