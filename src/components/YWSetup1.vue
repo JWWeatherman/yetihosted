5573a1b348460c5550f4cedc1d8bcb8f6e961188
Aaron Torres <aarontorres1423@gmail.com>
2022-06-11 19:30:34 -0500

    Yeti Level 2 only requires one laptop

--- src/components/YWSetup1.vue
+++ src/components/YWSetup1.vue
@@ -5,7 +5,7 @@
       <h2>Step 1: Gather Required Equipment</h2>
       <p>Gather the following items and then click <b>Next</b>.</p>
         <ul>
-        <li>A laptop that can run Ubuntu and Bitcoin Core. Almost any laptop made for Windows will work, but if your laptop has an SSD type hard drive some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD. You can also buy a new laptop from a store like Amazon or Walmart and return it when you are finished with setup because your bitcoin will be stored on paper and CD-ROM disks. Label one laptop "Primary" and the other "Secondary."</li>
+        <li>A laptop that can run Ubuntu and Bitcoin Core. Almost any laptop made for Windows will work, but if your laptop has an SSD type hard drive some steps will take a few hours instead of a few days. Laptops without SSD type drives cost about $150 USD and laptops with SSD drives cost about $200 USD. You can also buy a new laptop from a store like Amazon or Walmart and return it when you are finished with setup because your bitcoin will be stored on paper and CD-ROM disks.</li>
             <li>An optical drive to write to the CDs. Most laptops come with one built in, but you can also    buy an external optical drive for about $15 on Amazon.</li>
             <li>7 blank CD-R disks. Label the disks "Seed 1" "Seed 2" "Seed 3" through "Seed 7."</li>
             <li>2 USB thumb drives. These will cost about $3 each and will be used to install Ubuntu and to erase your laptop. Label one "Ubuntu" and the other "Transfer."</li>
