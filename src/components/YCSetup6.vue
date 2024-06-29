ba48eada4034c3fa697e06c7c1fc050d6acfc517
Chaz <chazupload@gmail.com>
2024-06-29 19:10:45 -0400

    Update YCSetup6.vue

--- src/components/YCSetup6.vue
+++ src/components/YCSetup6.vue
@@ -6,10 +6,10 @@
       <ol>
         <li>On your <b>Secondary</b> laptop click on the <b>9 Dots</b> in the bottom left corner of your Ubuntu desktop and then click <b>Terminal</b>.</li>
         <li>Click <b>Copy</b> to copy the text below this line.</li>
-        <div style="width:75%;" class="input-group mb-3 mt-3">
-        <input v-on:click="copy1" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy1" aria-describedby="inputGroup-sizing-default" value="sudo apt update && sudo apt-get upgrade -y && { grep -q "^VERSION=\"24.04" /etc/os-release && sudo apt-add-repository -y ppa:deadsnakes/ppa || true; } && sudo apt-get install -y python3.10 && sudo apt-get install -y python3.10-venv git && python3.10 -m venv ~/my_venv && source ~/my_venv/bin/activate && python -m ensurepip --upgrade && git clone https://github.com/jwweatherman/yeticold.git ~/yeticold; pip install flask; pip install bip32; python3 ~/yeticold/initialize.py YetiLevelThreeSecondaryCreate">
+        <div style="width: 75%;" class="input-group mb-3 mt-3">
+          <input v-on:click="copy1" v-b-tooltip.click v-b-tooltip.blur title="Copied" readonly type="text" class="form-control" aria-label="Default" id="copy1" aria-describedby="inputGroup-sizing-default" :value="command">
           <div class="input-group-append">
-            <span v-on:click="copy1" class="btn btn-secondary" id="basic-addon2">Copy</span>
+            <span v-on:click="copy1" class="btn btn-secondary" id="copyButton1">Copy</span>
           </div>
         </div>          
         <li>In the terminal window right click and select <b>Paste</b> and then click <b>Enter</b>.</li>
@@ -20,18 +20,25 @@
 </template>
 
 <script>
-import navbarcold from './navbarcold'
+import navbarcold from './navbarcold';
+
 export default {
-  methods: {
-    copy1 () {
-      let copyText = document.getElementById("copy1")
-      copyText.select()
-      copyText.setSelectionRange(0, 99999)
-      document.execCommand("copy")
-    }
-  },
   components: {
     navbarcold
+  },
+  data() {
+    return {
+      command: `sudo apt update && sudo apt-get upgrade -y && { grep -q "^VERSION=\"24.04\" /etc/os-release && sudo apt-add-repository -y ppa:deadsnakes/ppa || true; } && sudo apt-get install -y python3.10 && sudo apt-get install -y python3.10-venv git && python3.10 -m venv ~/my_venv && source ~/my_venv/bin/activate && python -m ensurepip --upgrade && git clone https://github.com/jwweatherman/yeticold.git ~/yeticold; pip install flask; pip install bip32; python3 ~/yeticold/initialize.py YetiLevelThreeSecondaryCreate`
+    };
+  },
+  methods: {
+    copy1() {
+      let copyText = document.getElementById("copy1");
+      if (copyText) {
+        copyText.select();
+        document.execCommand("copy");
+      }
+    }
   }
-}
+};
 </script>
